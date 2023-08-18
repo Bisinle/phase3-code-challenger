cc75aa7b25dfb3960b0e625236ccbe308a3fef9f
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-18 16:27:52 +0300

    Refactored the code, made it shorter  and got rid of two loops

--- Consonent-value.py
+++ Consonent-value.py
@@ -1,28 +1,19 @@
-# import re
-# no_vowels = re.sub("[aieou]", ",", string)
+import re
 
 
 def consonent_value(string):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    alpha_postion = {char: index for index, char in enumerate(alphabet)}
-    vowels = [("a", ","), ("e", ","), ("o", ","), ("i", ","), ("u", ",")]
-
-    string = "".join(
-        string.replace(char, vowel) for char, vowel in vowels if char in string
-    )
+    string = re.sub("[aeiou]", ",", string)
     grouped = [char for char in string.split(",") if char.isalpha()]
     grouped_list = list()
-    for g in grouped:
-        grouped_added = 0
-        if len(g) > 1:
-            for i in g:
-                grouped_added += alpha_postion.get(i) + 1
-            grouped_list.append(grouped_added)
-        else:
-            grouped_added += alpha_postion.get(g) + 1
-            grouped_list.append(grouped_added)
-
+    for element in grouped:
+        grouped_added = sum(alphabet.index(char) + 1 for char in element)
+        grouped_list.append(grouped_added)
     return max(grouped_list)
 
 
 print(consonent_value("strength"))
+
+
+# vowels = [("a", ","), ("e", ","), ("o", ","), ("i", ","), ("u", ",")]
+# string = "".join(string.replace(char, vowel) for char, vowel in vowels if char in string)
