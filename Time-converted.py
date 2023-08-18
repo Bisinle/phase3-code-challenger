141e91639ca54cbb8e7bc7088b1438a598c20f3d
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-18 16:48:34 +0300

    Refactored and deleted some useless code

--- Time-converted.py
+++ Time-converted.py
@@ -1,21 +1,15 @@
-def time_converter(hours_, minutes_, period):
-    min_to_sec = minutes_ * 60
-    hrs_to_secs = hours_ * 3600
-
-    # trun all into seconds
-    time_in_sconds = min_to_sec + hrs_to_secs
-    hours = time_in_sconds // 3600
-    minutes = (time_in_sconds % 3600) // 60
-
+def time_converter(hours, minutes, period):
     if period == "am" and hours > 11:
         return f"{hours-12:02d}:{minutes} "
-    elif period == "pm" and hours < 12:
-        return f"{hours + 12:02d}:{minutes} "
-    elif period == "pm" and hours > 11:
-        return f"{hours-12:02d}:{minutes} "
-
+    elif period == "pm":
+        if hours == 12:
+            return f"{hours:02d}:{minutes} "
+        elif hours < 12:
+            return f"{hours + 12:02d}:{minutes} "
+        elif hours > 11:
+            return f"{hours-12:02d}:{minutes} "
     else:
         return f"{hours:02d}:{minutes:02d} "
 
 
-print(time_converter(5, 50, "pm"))
+print(time_converter(1, 50, "am"))
