3e4576f551d976564f8946432ccba6a0e1baa413
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-18 17:02:51 +0300

    Refactored and deleted some useless code

--- Time-converted.py
+++ Time-converted.py
@@ -1,15 +1,18 @@
 def time_converter(hours, minutes, period):
-    if period == "am" and hours > 11:
-        return f"{hours-12:02d}:{minutes} "
-    elif period == "pm":
-        if hours == 12:
-            return f"{hours:02d}:{minutes} "
-        elif hours < 12:
-            return f"{hours + 12:02d}:{minutes} "
-        elif hours > 11:
+    if (period == "am" or period == "pm") and (minutes <= 59) and (0 <= hours <= 12):
+        if period == "am" and hours > 11:
             return f"{hours-12:02d}:{minutes} "
+        elif period == "pm":
+            if hours == 12:
+                return f"{hours:02d}:{minutes} "
+            elif hours < 12:
+                return f"{hours + 12:02d}:{minutes} "
+            elif hours > 11:
+                return f"{hours-12:02d}:{minutes} "
+        else:
+            return f"{hours:02d}:{minutes:02d} "
     else:
-        return f"{hours:02d}:{minutes:02d} "
+        return "NOT A VALID HOUR"
 
 
-print(time_converter(1, 50, "am"))
+print(time_converter(12, 50, "am"))
